e7e7bd9e59dcdeed96e6cc129d71841d3842da39
omaldonado22 <67865933+omaldonado22@users.noreply.github.com>
2020-07-05 09:34:58 -0600

    Repositorio POO

---
+++ src/main/java/hn/uth/poo/p2/primerrepositorio/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.uth.poo.p2.primerrepositorio;
+
+/**
+ *
+ * @author DELL
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi primer repositprio");
+        // TODO code application logic here
+    }
+    
+}
